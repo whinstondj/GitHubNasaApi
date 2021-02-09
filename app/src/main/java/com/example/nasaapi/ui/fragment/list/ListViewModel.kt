7909f9144b79bf1b0f9591db5ef6ccd815330df4
Winston <whinstondj@gmail.com>
2021-02-08 20:58:28 -0500

    f001 Coroutines

--- app/src/main/java/com/example/nasaapi/ui/fragment/list/ListViewModel.kt
+++ app/src/main/java/com/example/nasaapi/ui/fragment/list/ListViewModel.kt
@@ -1,6 +1,15 @@
 package com.example.nasaapi.ui.fragment.list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ListViewModel: ViewModel() {
+    fun requestInformation() {
+        viewModelScope.launch(Dispatchers.IO) {
+            
+        }
+    }
+
 }
