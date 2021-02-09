37366e34a7500b17ce96f722f46a25d146a4422f
Winston <whinstondj@gmail.com>
2021-02-08 20:47:19 -0500

    f001 Data Binding

--- app/src/main/java/com/example/nasaapi/ui/fragment/detail/DetailFragment.kt
+++ app/src/main/java/com/example/nasaapi/ui/fragment/detail/DetailFragment.kt
@@ -6,13 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.nasaapi.R
+import com.example.nasaapi.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+    lateinit var binding: FragmentDetailBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+
+        binding = FragmentDetailBinding.inflate(inflater, container,false)
+        return binding.root
     }
 }
--- app/src/main/java/com/example/nasaapi/ui/fragment/list/ListFragment.kt
+++ app/src/main/java/com/example/nasaapi/ui/fragment/list/ListFragment.kt
@@ -6,13 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.nasaapi.R
+import com.example.nasaapi.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+
+    lateinit var binding: FragmentListBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+
+        binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
